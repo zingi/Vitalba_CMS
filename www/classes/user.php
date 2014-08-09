bdfa790226cdbcc85a3b60a5707e868f371e93d1
fabiovitalba <fabvit9@gmail.com>
2014-08-09 19:46:26 +0200

    Commit #013
    - Updated User (More Information)
    - Updated CSS Standarts

--- www/classes/user.php
+++ www/classes/user.php
@@ -24,43 +24,40 @@
 
 		// Functions
 		function buildUserCard ()	{
-			/*
-			- VCMS_User
-			--- ID, INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-			--- Username, VARCHAR(10) NOT NULL
-			--- Password, VARCHAR(20) NOT NULL
-			--- Language_ID, INT NOT NULL FOREIGN KEY
-			--- Name, VARCHAR(20) NOT NULL
-			--- Surname, VARCHAR(20) NOT NULL
-			--- Birthdate, DATE NULL
-			--- Description, TEXT NULL
-			--- Sex, VARCHAR(1) NULL
-			--- Address_ID, INT NOT NULL FOREIGN KEY
-			--- Contact_ID, INT NOT NULL FOREIGN KEY
-			--- Join_Date, DATETIME NOT NULL
-			--- Last_Login, DATETIME NOT NULL
-			--- Premissionset_ID, INT NOT NULL FOREIGN KEY
-			--- Image_ID, INT NOT NULL FOREIGN KEY
-			--- SSL, BOOLEAN NOT NULL
-			*/
-			// To-Do: Fill in Captions from Form.php
-			//		  database->getUserAsArray()
+			// To-Do: database->getUserAsArray()
 			$user = $databaseConnection->getUserAsArray($id);
 			$caption = $databaseConnection->getCaptions("userCard");
 
 			$printString  = "<article class='vcms_usercard " + $css + "'>";
-			$printString += 	"<div class='vcms_usravatar'>" + $user['image'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['uname'] + ": </div><div class='vcms_usrvalue'>" + $user['username'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['name_'] + ": </div><div class='vcms_usrvalue'>" + $user['name'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['sname'] + ": </div><div class='vcms_usrvalue'>" + $user['surname'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['birth'] + ": </div><div class='vcms_usrvalue'>" + $user['birthdate'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['descr'] + ": </div><div class='vcms_usrvalue'>" + $user['description'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['sex__'] + ": </div><div class='vcms_usrvalue'>" + $user['sex'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['joind'] + ": </div><div class='vcms_usrvalue'>" + $user['joindate'] + "</div>";
-			$printString += 	"<div class='vcms_usrlabel'>" + $caption['lasto'] + ": </div><div class='vcms_usrvalue'>" + $user['lastlogin'] + "</div>";
-			
-			// To-Do: If the user is watching his own card, display all information.
-			//		  If enabled, show Adress/Contact information
+
+			$printString += 	"<div class='vcms_usrmaininfo'>":
+			$printString += 		"<div class='vcms_usravatar'>" + $user['image'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['uname'] + ": </div><div class='vcms_usrvalue'>" + $user['username'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['name_'] + ": </div><div class='vcms_usrvalue'>" + $user['name'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['sname'] + ": </div><div class='vcms_usrvalue'>" + $user['surname'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['birth'] + ": </div><div class='vcms_usrvalue'>" + $user['birthdate'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['descr'] + ": </div><div class='vcms_usrvalue'>" + $user['description'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['sex__'] + ": </div><div class='vcms_usrvalue'>" + $user['sex'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['joind'] + ": </div><div class='vcms_usrvalue'>" + $user['joindate'] + "</div>";
+			$printString += 		"<div class='vcms_usrlabel'>" + $caption['lasto'] + ": </div><div class='vcms_usrvalue'>" + $user['lastlogin'] + "</div>";
+			$printString += 	"</div>";
+
+			$printString +=		"<div class='vcms_usraddressinfo'>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['stree'] + ": </div><div class='vcms_usrvalue'>" + $user['street1'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['stre2'] + ": </div><div class='vcms_usrvalue'>" + $user['street2'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['house'] + ": </div><div class='vcms_usrvalue'>" + $user['houseno'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['posta'] + ": </div><div class='vcms_usrvalue'>" + $user['postalcode'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['city_'] + ": </div><div class='vcms_usrvalue'>" + $user['city'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['provi'] + ": </div><div class='vcms_usrvalue'>" + $user['provincecode'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['regio'] + ": </div><div class='vcms_usrvalue'>" + $user['region'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['count'] + ": </div><div class='vcms_usrvalue'>" + $user['countrycode'] + "</div>";
+			$printString +=		"</div>";
+
+			$printString +=		"<div class='vcms_usrcontactinfo'>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['email'] + ": </div><div class='vcms_usrvalue'>" + $user['email'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['telep'] + ": </div><div class='vcms_usrvalue'>" + $user['telephone'] + "</div>";
+			$printString +=			"<div class='vcms_usrlabel'>" + $caption['fax__'] + ": </div><div class='vcms_usrvalue'>" + $user['fax'] + "</div>";
+			$printString +=		"</div>";
 
 			$printString += "</article>";
 		}
